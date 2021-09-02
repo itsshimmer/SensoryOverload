768b1c2141fa581ac07c11fb4dedb86d4cab86b6
João Brentano <joaobrentano.br@gmail.com>
2021-09-02 18:58:01 -0300

    Xcode project creation

---
+++ SensoryOverload/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SensoryOverload
+//
+//  Created by João Brentano on 02/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SensoryOverload/SensoryOverloadApp.swift
@@ -0,0 +1,17 @@
+//
+//  SensoryOverloadApp.swift
+//  SensoryOverload
+//
+//  Created by João Brentano on 02/09/21.
+//
+
+import SwiftUI
+
+@main
+struct SensoryOverloadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
